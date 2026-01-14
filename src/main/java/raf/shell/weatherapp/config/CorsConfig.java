012e38f92dda251c86cc5b1fca57a47914733dcc
Milan Bojic <mbojic12@gmail.com>
2026-01-14 20:54:10 +0100

    fixed cross origins for localhost. Not for production.

--- src/main/java/raf/shell/weatherapp/config/CorsConfig.java
+++ src/main/java/raf/shell/weatherapp/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:4201"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:4201", "http://localhost"));
         config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
